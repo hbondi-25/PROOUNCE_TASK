deb44275465a7f137077f04ded77a3522104ed63
hbondi-25 <=>
2025-11-10 18:37:20 +0530

    initial Commit.

---
+++ PROOUNCE_TASK/src/main/java/com/example/PROOUNCE_TASK/ProounceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.PROOUNCE_TASK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProounceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProounceTaskApplication.class, args);
+	}
+
+}
---
+++ PROOUNCE_TASK/src/test/java/com/example/PROOUNCE_TASK/ProounceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROOUNCE_TASK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProounceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
